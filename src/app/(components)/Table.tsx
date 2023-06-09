91350223db1d797b0122897a4dab4d2449790629
hasan-hayder <hasan.heidar2001@gmail.com>
2023-06-09 15:14:08 +0400

    table sorting

--- src/app/(components)/Table.tsx
+++ src/app/(components)/Table.tsx
@@ -1,37 +1,102 @@
-import { Fragment } from "react"
+import { Fragment, useState } from "react";
+import { GoChevronUp, GoChevronDown } from "react-icons/go";
 
 interface TableProps {
-    data: any[],
-    config: any[]
+  data: any[];
+  config: any[];
 }
 
-export default function Table({data, config}: TableProps) {
-const renderedHeaders = config.map((column) => {
+export default function Table({ data, config }: TableProps) {
+  const [sortColumn, setSortColumn] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
+
+  const handleSort = (columnKey: any) => {
+    if (sortColumn === columnKey) {
+      // Toggle sorting order if the same column is clicked
+      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    } else {
+      // Set new sorting column and default sorting order
+      setSortColumn(columnKey);
+      setSortOrder("asc");
+    }
+  };
+
+  const renderSortArrow = () => {
+    if (sortOrder === "asc") {
+      return (
+        <span>
+          <GoChevronUp />{" "}
+        </span>
+      ); // Upward-pointing arrow
+    } else {
+      return (
+        <span>
+          {" "}
+          <GoChevronDown />{" "}
+        </span>
+      ); // Downward-pointing arrow
+    }
+  };
+
+  const renderedHeaders = config.map((column) => {
     if (column.header) {
-        return <Fragment key={column.key}>{column.label}</Fragment>
+      return <Fragment key={column.key}>{column.label}</Fragment>;
     }
     return (
-        <th className="border-r p-2 border-burgundy" key={column.key}>{column.label}</th>
-    )
-})
+      <th
+        className="border-r p-2 border-burgundy"
+        key={column.key}
+        onClick={() => handleSort(column.key)}
+      >
+        <div className="flex gap-2 items-center cursor-pointer">
+          {sortColumn === column.key && renderSortArrow()} {column.label}
+        </div>
+      </th>
+    );
+  });
 
-const renderedRows = data.map((rowData) => {
-    const renderedCells = config.map((column) => {
+  const renderedRows = [...data]
+    .sort((a, b) => {
+      const aValue = a[sortColumn];
+      const bValue = b[sortColumn];
 
-        return <td className="p-2" key={column.key}>{column.render(rowData)}</td>
+if (typeof aValue !== "undefined" && typeof bValue !== "undefined") {
+      if (typeof aValue === "string" && typeof bValue === "string") {
+        if (sortOrder === "asc") {
+          return aValue.localeCompare(bValue);
+        } else {
+          return bValue.localeCompare(aValue);
+        }
+      } else {
+        if (sortOrder === "asc") {
+          return aValue - bValue;
+        } else {
+          return bValue - aValue;
+        }
+      }
+    }
+    return 0
     })
+    .map((rowData) => {
+      const renderedCells = config.map((column) => {
+        return (
+          <td className="p-2" key={column.key}>
+            {column.render(rowData)}
+          </td>
+        );
+      });
 
-    return (
-        <tr  key={rowData.id}>{renderedCells}</tr>
-    )
-})
+      return <tr key={rowData.id}>{renderedCells}</tr>;
+    });
 
-    return (
-        <table className="table-auto  bg-logo  rounded-md">
-            <thead>
-                <tr className="border-b-8 border-b-burgundy text-forest-green">{renderedHeaders}</tr>
-            </thead>
-            <tbody>{renderedRows}</tbody>
-        </table>
-    )
+  return (
+    <table className="table-auto  bg-logo  rounded-md">
+      <thead>
+        <tr className="border-b-8 border-b-burgundy text-forest-green">
+          {renderedHeaders}
+        </tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
