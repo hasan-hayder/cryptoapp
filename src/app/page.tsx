66ba4cf0505a3de39689af79f64f285f3f74ccf7
hasan-hayder <hasan.heidar2001@gmail.com>
2023-06-20 10:10:18 +0400

    small style change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,16 @@ export default function Home() {
   return (
     <div className="flex-auto py-20 sm:py-40 px-2 sm:px-10 justify-center mx-auto items-center">
       <div className="text-burgundy">
-        <h1 className="uppercase text-3xl sm:text-7xl my-5">
-          Buy Bitcoin and Cryptocurrencies
-        </h1>
+        <h1>Buy Bitcoin and Cryptocurrencies</h1>
         <p className="text-3xl sm:text-4xl">Register now and start trading.</p>
       </div>
       <div>
         <form className="my-8 h-12 flex">
-          <input type="email" className="mr-5 border-2 rounded-lg px-5 focus:outline-none focus:border-burgundy focus:ring-burgundy" placeholder="Email Address"></input>
+          <input
+            type="email"
+            className="mr-5 border-2 rounded-lg px-5 focus:outline-none focus:border-burgundy focus:ring-burgundy"
+            placeholder="Email Address"
+          ></input>
           <Button register>Get Started</Button>
         </form>
       </div>
